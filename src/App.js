2707038db435eff304c98987a1817ff72923a979
JuanDEV <juan_aguilar1290@outlook.es>
2022-12-11 00:10:55 -0600

    apiCall test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { apiCall } from './helpers/apiCall';
 
 function App() {
+
+  const [reponse, setReponse] = useState(null);
+
+  useEffect(() => {
+    const response = async () => {
+      await apiCall({urlParam: 'characters'}).then((data) => {
+        setReponse(data.data);
+      });
+    }
+
+    response();
+  }, []);
+
   return (
     <div className="App">
-    
+    {
+      reponse && console.log(reponse)
+    }
     </div>
   );
 }
