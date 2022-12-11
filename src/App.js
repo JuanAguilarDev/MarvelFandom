16f0b8561335d85c8f1a9edc87cebc91d71aad6a
AlexxCepeda <acepeda1800@alumno.ipn.mx>
2022-12-10 22:27:50 -0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import NavigationBar from './components/Navbar/Navbar';
+import Home from './components/pages/Home';
+import Test from './components/pages/Test';
 
 function App() {
   return (
     <div className="App">
-    
+      <NavigationBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/test' element={<Test />} />
+      </Routes>
     </div>
   );
 }
