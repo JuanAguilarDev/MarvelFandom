410e7b5f91d2cbc975e50b8c2bb6aa85d3bd5fe2
JuanDEV <juan_aguilar1290@outlook.es>
2022-12-12 16:51:18 -0600

    Hero added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import { apiCall } from './helpers/apiCall';
 import NavigationBar from './components/Navbar/Navbar';
 import Home from './components/pages/Home';
 import Test from './components/pages/Test';
+import { Footer } from './components/Footer/Footer';
+import { Hero } from './components/Hero/Hero';
 
 function App() {
 
   // ApiCall use example
-  const [reponse, setReponse] = useState(null);
+  const [response, setResponse] = useState(null);
 
   useEffect(() => {
     const response = async () => {
       await apiCall({urlParam: 'characters'}).then((data) => {
-        setReponse(data.data);
+        setResponse(data.data);
       });
     }
 
@@ -24,15 +26,18 @@ function App() {
 
   return (
     <div className="App">
-    // ApiCall data example
+    
     {
-      reponse && console.log(reponse)
+      // ApiCall data example
+      response && console.log(response)
     }
       <NavigationBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/test' element={<Test />} />
       </Routes>
+      <Hero></Hero>
+      <Footer></Footer>
     </div>
   );
 }
